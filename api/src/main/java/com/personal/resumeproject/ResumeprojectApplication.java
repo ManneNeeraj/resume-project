7919a8e7ec6fb1fa911c4c7a76ec7364ced5d180
ManneNeeraj <neerajmanne111@gmail.com>
2025-11-12 19:14:37 +0530

    setup completed commit

---
+++ api/src/main/java/com/personal/resumeproject/ResumeprojectApplication.java
@@ -0,0 +1,14 @@
+package com.personal.resumeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeprojectApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ResumeprojectApplication.class, args);
+	}
+
+}
---
+++ api/src/main/java/com/personal/resumeproject/model/Users.java
@@ -0,0 +1,38 @@
+package com.personal.resumeproject.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Users {
+
+    @Id
+    @GeneratedValue
+    private int userId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+
+    private String phone;
+
+
+}
